b8de3953f5d89f5030b208d141796c4a466d0dd0
morandave <100688146+morandave@users.noreply.github.com>
2025-07-11 11:22:19 +0800

    implement scaled dot-product attention and positional encoding logic

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -9,7 +9,29 @@ def scaled_dot_product_attention_simple(
     scale: float | None = None,
     mask: mx.array | None = None,
 ) -> mx.array:
-    pass
+    """
+    Computes the scaled dot-product attention.
+
+    Args:
+        query: Query tensor of shape (bs, H, L, D).
+        key: Key tensor of shape (bs, H, L, D).
+        value: Value tensor of shape (bs, H, L, D).
+        scale: Scaling factor for the dot product.
+        mask: Optional mask tensor to apply to the attention scores.
+    Returns:
+        Output tensor after applying attention.
+    """
+    k_T = mx.transpose(key, axes=(0, 1, 3, 2))  # (bs, H, L, D) -> (bs, H, D, L)
+    scores = mx.matmul(query, k_T)  # (bs, H, L, L)
+    if scale is not None:
+        scores = scores * scale  # Scale the scores
+    if mask is not None:
+        if isinstance(mask, str):
+            mask = mx.array(mask)
+        scores = scores + mask  # Apply the mask
+    scores = softmax(scores, axis=-1)  # Normalize the scores
+    output = mx.matmul(scores, value)  # (bs, H, L, D)
+    return output
 
 
 class SimpleMultiHeadAttention:
@@ -22,7 +44,14 @@ class SimpleMultiHeadAttention:
         wv: mx.array,
         wo: mx.array,
     ):
-        pass
+        """
+        wq, wk, wv:(hidden_size, (num_heads*D) )
+        wo:((num_heads*D), hidden_size)
+        """
+        self.pre_q_linear = linear(wq)
+        self.pre_k_linear = linear(wk)
+        self.pre_v_linear = linear(wv)
+        self.post_linear = linear(wq)
 
     def __call__(
         self,
@@ -31,7 +60,20 @@ class SimpleMultiHeadAttention:
         value: mx.array,
         mask: mx.array | None = None,
     ) -> mx.array:
-        pass
+        '''
+        query:(bs,L,hidden_size)
+
+        return;
+        output:(bs, )
+        '''
+        scale = mx.sqrt(query.shape[-1])
+        query = self.pre_q_linear(query) # (bs,L,hidden_size) -> (bs, L, H, D)
+        query = mx.transpose(query, axis=(0,2,1,3)) # (bs, L, H, D) -> (bs, H, L, D)
+        # ……
+        attention = scaled_dot_product_attention_simple(query, key, value, scale, mask)
+        attention = mx.transpose(attention, axis=(0,2,1,3))
+        output = self.post_linear(attention) # ->(bs,L,hidden_size)
+        return output
 
 
 def causal_mask(L: int, S: int, dtype: mx.Dtype) -> mx.array:
--- src/tiny_llm/positional_encoding.py
+++ src/tiny_llm/positional_encoding.py
@@ -9,9 +9,17 @@ class RoPE:
         base: int = 10000,
         traditional: bool = False,
     ):
-        pass
+        self.dims = dims
 
     def __call__(
         self, x: mx.array, offset: list[slice] | slice | None = None
     ) -> mx.array:
-        pass
+        D_2 = self.dims / 2 # resume dims为偶数
+
+        # 根据D_2创建旋转矩阵
+        rotate = None
+
+        # 变换x维度 (B, L, H, D)->(B, H, L, D)
+        output = rotate * x
+
+        return output
